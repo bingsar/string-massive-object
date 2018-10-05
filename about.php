31a53a39a449fba7eb7ea8df0f3c60ff4ec08869
Anton Shmigelskiy <tony.shmig@gmail.com>
2018-10-05 21:01:06 +0300

    Проверьте пожалуйста

---
+++ about.php
@@ -0,0 +1,50 @@
+<?php
+$name = 'Антон Шмигельский';
+$age = 29;
+$email = 'tony.shmig@gmail.com ';
+$city = 'Санкт-Петербург';
+$about = 'junior php';
+?>
+
+<!DOCTYPE>
+<html>
+<head>
+    <title>Меня зовут <?= $name ?></title>
+    <meta charset="utf-8">
+    <style>
+        body {
+            font-family: sans-serif;
+        }
+        p {
+            text-align: center;
+        }
+        h1 {
+            text-align: center;
+        }
+        h2 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<h1>Небольшая справка</h1>
+<p>
+    <br>
+    <br>
+
+</p>
+<h2>Данные пользователя системы - <?= $name ?></h2>
+<dl>
+    <dt>Имя</dt>
+    <dd><?= $name ?></dd>
+    <dt>Возраст</dt>
+    <dd><?= $age ?></dd>
+    <dt>Адрес электронной почты</dt>
+    <dd><a href="mailto:<?= $email ?>"><?= $email ?></a></dd>
+    <dt>Город</dt>
+    <dd><?= $city ?></dd>
+    <dt>О себе</dt>
+    <dd><?= $about ?></dd>
+</dl>
+</body>
+</html>
