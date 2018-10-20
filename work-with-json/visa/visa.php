5f0cff4fc2ffb14eba914d15fcfce9a9fa997247
Anton Shmigelskiy <43887830+bingsar@users.noreply.github.com>
2018-10-20 21:02:30 +0300

    encoding

--- work-with-json/visa/visa.php
+++ work-with-json/visa/visa.php
@@ -2,12 +2,13 @@
 $csvName = 'data.csv';
 $handle = fopen($csvName, 'r');
 $csv = array_map('str_getcsv', file($csvName));
+print_r($csv);
 $shortest = -1;
 if (isset($argv[1])) {
     $input = $argv[1];
-foreach ($csv as $word) {
-    $lev = levenshtein($input, $word[1]);
-            if ($lev == 0) {
+    foreach ($csv as $word) {
+        $lev = levenshtein($input, $word[1]);
+        if ($lev == 0) {
             $closest = $word[1];
             $shortest = 0;
             break;
@@ -17,13 +18,13 @@ foreach ($csv as $word) {
             $shortest = $lev;
         }
     }
-if ($shortest == 0) {
-        if ($word[1] == $argv[1]) {
-            echo $closest . ': ' . $word[4];
-        }
-    } else {
-    echo "Did you mean?: $closest\n";
-    }
-} else {
-    echo 'Please type right country name';
+    echo $closest . ': ' . $word[4];
 }
+//if ($shortest == 0) {
+//        if ($word[1] == $argv[1]) {
+//
+//        }
+//
+// else {
+//    echo 'Please type right country name';
+//}
